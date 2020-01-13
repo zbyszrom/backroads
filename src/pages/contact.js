a9ca8f2e14fee37b3740cdec08484eaf95f425cd
Zbigniew Downar <zbyszrom@Zbigniews-iMac.local>
2020-01-14 00:16:41 +0100

    contact stub

---
+++ src/pages/contact.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+const Contact = () => (
+    <Layout>
+     
+      <h1>Hi from contact page</h1>
+      
+    </Layout>
+  )
+  
+  export default Contact
+
--- src/pages/tours.js
+++ src/pages/tours.js
@@ -1,16 +1,16 @@
 import React from "react"
-import { Link } from "gatsby"
+
 
 import Layout from "../components/layout"
 
 
-const SecondPage = () => (
+const Tours = () => (
   <Layout>
    
     <h1>Hi from tours page</h1>
     
-    <Link to="/">Go back to the homepage</Link>
+    
   </Layout>
 )
 
-export default SecondPage
+export default Tours
