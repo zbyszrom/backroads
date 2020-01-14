a03a9730388b1c029750928c5899a44f0891a9de
Zbigniew Downar <zbyszrom@Zbigniews-iMac.local>
2020-01-14 14:19:28 +0100

    about & services

--- src/components/about.js
+++ src/components/about.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import img from "../images/defaultBcg.jpg"
-
+import styles from './about.module.scss'
 const About =()=>
 {
     return (
@@ -8,7 +8,27 @@ const About =()=>
         <div className='title'>
         <h4>About <span>Us</span></h4>
         </div>
-        
+        <div className={styles.center}>
+        <article className={styles.img}>
+        <div className={styles.imgContainer}>
+        <img src={img} alt="about company" />
+        </div>
+        </article>
+        <article className={styles.info}>
+        <h4>explore the difference</h4>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla
+            doloribus enim necessitatibus?
+          </p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla
+            doloribus enim necessitatibus?
+          </p>
+          <button type="button" className="btn-primary">
+            read more
+          </button>  
+        </article>
+        </div>
         </section>
     )
 }
--- src/components/services.js
+++ src/components/services.js
@@ -1,13 +1,25 @@
 import React from 'react'
+import services from "../constants/services"
+import styles from "./services.module.scss"
 
 const Services =()=>
 {
     return (
-        <section>
+        <section style={{background: 'var(--mainGrey)'}}>
         <div className='title'>
         <h4>Our <span>Services</span></h4>
         </div>
-        
+        <div className={styles.center}>
+        {services.map((item, index) => {
+          return (
+            <article key={index} className={styles.service}>
+              <span>{item.icon}</span>
+              <h4>{item.title}</h4>
+              <p>{item.text}</p>
+            </article>
+          )
+        })}
+        </div>
         </section>
     )
 }
